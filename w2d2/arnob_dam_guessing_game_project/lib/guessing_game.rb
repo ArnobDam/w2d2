ecef68ab5f3fd15a42569d1c2fb66aa09b87de7d
Arnob <arnobdam@gmail.com>
2022-09-20 09:45:18 -0400

    6/18 specs in guessing_game passed

---
+++ w2d2/arnob_dam_guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,18 @@
+class GuessingGame
+
+    attr_reader :game_over
+
+    def initialize(min, max)
+        @min = min
+        @max = max
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    def num_attempts
+        @num_attempts
+    end
+
+
+end
